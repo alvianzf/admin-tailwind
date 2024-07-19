6ead472f5e60746fc33398b018e378d8cd8b36c2
alvianzf <alvianzf@gmail.com>
2024-07-20 00:01:13 +0700

    deploy: change to npx from serve

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,8 +3,9 @@ module.exports = {
     apps: [
       {
         name: 'admin',
-        script: 'serve',
-        args: '-s build -l 3030',
+        script: 'npx',
+        args: 'serve -s build -l 3030',
+        interpreter: 'none',
         env: {
           NODE_ENV: 'production',
         },
