c4253ca0151ec4ec6fd11b7b23e17cf240210c7b
alvianzf <alvianzf@gmail.com>
2024-07-19 22:54:30 +0700

    deploy: change ecosystem script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,8 @@ module.exports = {
     apps: [
       {
         name: 'admin',
-        script: 'set PORT=3030 && react-scripts start',
-        // args: '-s build -l 3030',
+        script: 'serve',
+        args: '-s build -l 3030',
         isntances: 1,
         exec_mode: 'fork',
         env: {
